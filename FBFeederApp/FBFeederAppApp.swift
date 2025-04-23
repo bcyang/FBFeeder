c51e3fde4e051a73e14f4bae74bd7021af1e6380
Jerry Yang <jerry@mist.com>
2025-04-22 20:09:04 -0700

    Initial Commit

---
+++ FBFeederApp/FBFeederAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FBFeederAppApp.swift
+//  FBFeederApp
+//
+//  Created by bcyang on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FBFeederAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
